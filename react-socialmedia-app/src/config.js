b4ea0733732dd9185fd10547811c90669b6fae4b
Debesh Mohapatra <78497229+debesh-26@users.noreply.github.com>
2024-09-12 00:22:08 +0530

    Update config.js

--- react-socialmedia-app/src/config.js
+++ react-socialmedia-app/src/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8800/api/",
+    baseURL: "https://socialmedia-production-81c5.up.railway.app/api/",
 });
 
